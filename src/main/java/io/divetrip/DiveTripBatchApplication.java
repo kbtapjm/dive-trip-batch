19b054786137c6cec57ee4c78e963617992ca18f
jmpark <kbtapjm@gmail.com>
2025-01-07 10:35:40 +0900

    first commit

---
+++ src/main/java/io/divetrip/DiveTripBatchApplication.java
@@ -0,0 +1,13 @@
+package io.divetrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveTripBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveTripBatchApplication.class, args);
+	}
+
+}
